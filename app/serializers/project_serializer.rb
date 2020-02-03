62ea606f0efedfaf64646d539f83a9d8e20b546a
github username <harr.hughes@gmail.com>
2020-02-03 12:43:13 +0000

    fixed token

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,3 +1,3 @@
 class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :name, :project_code, :description, :open
+  attributes :id, :name, :project_code, :description, :open, :updated_at
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :user_code, :token
+
+  def token
+    object.token
+  end
 end
